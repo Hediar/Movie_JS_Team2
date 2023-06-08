3d57cc7101caef5c66b9a3f2816d5e4e6ced476d
HojinLim <69897998+HojinLim@users.noreply.github.com>
2023-06-08 11:25:18 +0900

    나라별 분류
    굿

---
+++ src/apikey.js
@@ -0,0 +1,3 @@
+export const aut ='Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZmU0ZmQ2OTExMjk4YTk1MGI3ODhlNjk0ODVkZjZmZiIsInN1YiI6IjY0NzU4M2Y1Njc0M2ZhMDExOTdhNzA1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.exgR1edP5kPFmM8OjkVJfy8DoRlmQMJV4bQ_Msz6pBc';
+export const apiUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+
